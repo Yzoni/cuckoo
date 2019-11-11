81054c389864c0f9089dedb76e06f08adfa83a1f
Yzoni <mail@yorickdeboer.nl>
2019-11-11 19:27:14 +0100

    Test

--- cuckoo/data/analyzer/windows/modules/auxiliary/eventlog.py
+++ cuckoo/data/analyzer/windows/modules/auxiliary/eventlog.py
@@ -1,18 +1,21 @@
 # Copyright (C) 2019 - Y. de Boer
+import logging
 
+import os
 import os.path
 
 from lib.common.abstracts import Auxiliary
 from lib.common.exceptions import CuckooDisableModule, CuckooPackageError
 from lib.common.results import upload_to_host
 
+log = logging.getLogger(__name__)
 
 class Eventlog(Auxiliary):
     def start(self):
         self.key = "eventlog"
 
         self.eventlog_path = "/Windows/System32/Winevt/Logs/System.evtx"
-
+        
         # if not os.path.exists("/Windows/System32/"):
         #     raise CuckooPackageError(
         #         "{} could not be found.".format("/Windows/System32/")
@@ -26,6 +29,9 @@ class Eventlog(Auxiliary):
         #         "{} could not be found. {}. {}.".format(self.eventlog_path, contentsa, contentsb)
         #     )
         # Upload the EVTX file to the host.
+        
         upload_to_host("/Windows/System32/Winevt/Logs/Setup.evtx", os.path.join("files", "Setup.evtx"))
-
+        log.info("=======================")
+        log.info(os.getcwd())
+        log.info(os.path.dirname(os.path.abspath(__file__)))
         upload_to_host(self.eventlog_path, os.path.join("files", "System.evtx"))
