6e4740eef435fe33b201c7d8a3782ef585eedd53
Yzoni <mail@yorickdeboer.nl>
2019-11-15 14:41:31 +0100

    Do not stop processing eventlog when not yet empty

--- cuckoo/auxiliary/eventlogreceiver.py
+++ cuckoo/auxiliary/eventlogreceiver.py
@@ -3,6 +3,7 @@ from winlogbeatserver import winlogbeatserver
 import logging
 from cuckoo.misc import cwd
 import os
+import time
 
 log = logging.getLogger(__name__)
 
@@ -27,7 +28,12 @@ class EventlogReceiver(Auxiliary):
         """
         Stop the custom Winlogbeat receiver server
         """
-        logging.info('Current Winlogbeat processing queue still contained #{} to be processed items.'.format(
+        logging.info('Current Winlogbeat processing queue still contains {} items to be processed.'.format(
             self.wlb.queue_size()))
-        logging.info('Stopping Winlogbeat server anyway...')
+        
+        while self.wlb.queue_size() > 0:
+            logging.info('Winlogbeat queue stil not empty, {} items remaining.'.format(self.wlb.queue_size()))
+            time.sleep(5)
+        
+        logging.info('Queue empty... {} items remaining'.format(self.wlb.queue_size()))
         self.wlb.stop()
