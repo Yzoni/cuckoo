06d5a56558a19ca5167fb5a4bdce7ecc1d9d8a24
Yzoni <mail@yorickdeboer.nl>
2019-11-27 16:22:52 +0100

    Do not wait forever on eventlog queue

--- cuckoo/auxiliary/eventlogreceiver.py
+++ cuckoo/auxiliary/eventlogreceiver.py
@@ -32,9 +32,11 @@ class EventlogReceiver(Auxiliary):
         logging.info('Current Winlogbeat processing queue still contains {} items to be processed.'.format(
             self.wlb.queue_size()))
 
-        while self.wlb.queue_size() > 0:
+        stop_counter = 0
+        while self.wlb.queue_size() > 0 and stop_counter < 5:
             logging.info('Winlogbeat queue still not empty, {} items remaining.'.format(self.wlb.queue_size()))
             time.sleep(5)
+            stop_counter += 1
 
         logging.info('Queue empty... {} items remaining'.format(self.wlb.queue_size()))
         self.wlb.stop(compress=True)
